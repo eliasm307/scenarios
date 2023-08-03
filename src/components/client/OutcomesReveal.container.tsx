c22f8ade7a3917e8ae7c7207f551ca16a448e73a
Eli <13420359+eliasm307@users.noreply.github.com>
2023-08-03 02:59:35 +0100

    add user ready functionality to outcomes reveal

--- src/components/client/OutcomesReveal.container.tsx
+++ src/components/client/OutcomesReveal.container.tsx
@@ -4,6 +4,7 @@ import type { SessionRow, SessionUser } from "../../types";
 import type { BroadcastFunction } from "./GameSession";
 import { invokeResetSessionAction } from "../../utils/server/actions";
 import { useCustomToast } from "../../utils/client/hooks";
+import APIClient from "../../utils/client/APIClient";
 
 type Props = {
   currentUser: SessionUser;
@@ -17,16 +18,48 @@ type Props = {
 function useLogic({ users, outcomeVotes, scenarioText, currentUser, sessionId, broadcast }: Props) {
   const toast = useCustomToast();
   const handlePlayAgain = useCallback(async () => {
+    const readyToRestartKey = createReadyToRestartKey(currentUser.id);
+    const latestOutcomeVotes = {
+      ...outcomeVotes,
+      [currentUser.id]: {
+        ...outcomeVotes[currentUser.id],
+        [readyToRestartKey]: true,
+      },
+    };
+
+    if (everyUserIsReadyToRestart({ latestOutcomeVotes, users })) {
+      broadcast({
+        event: "Toast",
+        data: {
+          status: "info",
+          title: `Everyone is ready, restarting the session...`,
+        },
+      });
+      const errorToastConfig = await invokeResetSessionAction(sessionId);
+      errorToastConfig?.forEach(toast);
+      return;
+    }
+
+    // not everyone is ready to restart
     broadcast({
       event: "Toast",
       data: {
         status: "info",
-        title: `"${currentUser.name}" re-started the session`,
+        title: `"${currentUser.name}" is ready to re-start the session`,
       },
     });
-    const errorToastConfig = await invokeResetSessionAction(sessionId);
-    errorToastConfig?.forEach(toast);
-  }, [broadcast, currentUser.name, sessionId, toast]);
+
+    const errorToastConfig = await APIClient.sessions.voteForUserOutcome({
+      session_id: sessionId,
+      vote_by_user_id: currentUser.id,
+      vote_for_user_id: readyToRestartKey,
+      outcome: true,
+    });
+    if (errorToastConfig) {
+      console.error("outcomeVoteFromCurrentUser errorToastConfig", errorToastConfig);
+      toast(errorToastConfig);
+    }
+  }, [broadcast, currentUser.id, currentUser.name, outcomeVotes, sessionId, toast, users]);
 
   return {
     handlePlayAgain,
@@ -37,6 +70,25 @@ function useLogic({ users, outcomeVotes, scenarioText, currentUser, sessionId, b
 }
 
 export type OutcomesRevealViewProps = ReturnType<typeof useLogic>;
+
+function createReadyToRestartKey(userId: string) {
+  return `${userId}-ready-to-restart`;
+}
+
+function everyUserIsReadyToRestart({
+  latestOutcomeVotes,
+  users,
+}: {
+  latestOutcomeVotes: Required<SessionRow["scenario_outcome_votes"]>;
+  users: SessionUser[];
+}) {
+  return users.every((user) => {
+    const key = createReadyToRestartKey(user.id);
+    const userVoteMap = latestOutcomeVotes[user.id];
+    const isReadyToRestart = !!userVoteMap?.[key];
+    return isReadyToRestart;
+  });
+}
 
 export default function OutcomesRevealContainer(props: Props) {
   const viewProps = useLogic(props);

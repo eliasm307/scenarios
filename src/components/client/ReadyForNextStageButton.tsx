443ba0d042856c23906eb64bb23e3edd8a09a92e
Eli <13420359+eliasm307@users.noreply.github.com>
2023-08-03 02:57:17 +0100

    add ReadyForNextStageButton

--- src/components/client/ReadyForNextStageButton.tsx
+++ src/components/client/ReadyForNextStageButton.tsx
@@ -1,12 +1,18 @@
 import { Button } from "@chakra-ui/react";
 
+export type ReadyForNextStageButtonProps = {
+  /** Whether the user clicked the button and is ready for the next stage */
+  isReadyForNextStage: boolean;
+  handleReadyForNextStageClick: () => void;
+  /** For before clicking the button, should the user be able to click it? */
+  canMoveToNextStage: boolean;
+};
+
 export default function ReadyForNextStageButton({
-  isReady,
-  handleReady,
-}: {
-  isReady: boolean;
-  handleReady: () => void;
-}) {
+  isReadyForNextStage: isReady,
+  handleReadyForNextStageClick: handleReady,
+  canMoveToNextStage,
+}: ReadyForNextStageButtonProps) {
   return isReady ? (
     <Button key='ready' p={5} colorScheme='gray' isDisabled>
       Waiting for Other Players...
@@ -15,8 +21,8 @@ export default function ReadyForNextStageButton({
     <Button
       key='not-ready'
       p={5}
-      colorScheme={isReady ? "gray" : "green"}
-      isDisabled={isReady}
+      colorScheme={isReady || !canMoveToNextStage ? "gray" : "green"}
+      isDisabled={isReady || !canMoveToNextStage}
       onClick={handleReady}
     >
       I&apos;m Ready for the Next Stage

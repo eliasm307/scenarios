36ed7f8a340adae66b3ac1fe6520d7e48056f308
Eli <13420359+eliasm307@users.noreply.github.com>
2023-08-15 22:44:35 +0100

    add toolbar to auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,21 @@
 import AuthForm from "../../components/client/AuthForm";
+import { Grid, VStack } from "../../components/client/ChakraUI";
+import NavBar from "../../components/client/NavBar";
 
 export default function Home() {
-  return <AuthForm />;
+  return (
+    <Grid
+      minHeight={{ md: "100dvh" }}
+      height={{ base: "100dvh", md: undefined }}
+      overflow='hidden'
+      templateRows='auto 1fr'
+      position='fixed'
+      inset={0}
+    >
+      <NavBar zIndex={2} />
+      <VStack as='section' className='game-session-container' zIndex={1} overflow='hidden'>
+        <AuthForm />
+      </VStack>
+    </Grid>
+  );
 }
